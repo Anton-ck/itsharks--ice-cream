df927cfa92057b9dfd2e5dbca1e1c77c1edf915c
Anton-ck <114865363+Anton-ck@users.noreply.github.com>
2022-12-25 21:16:33 +0000

    Deploying to gh-pages from @ Anton-ck/itsharks--ice-cream@b06108e9b29d5c7d3470e85187c967651bbf58d3 🚀

---
+++ index.0d2dd661.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-buy-now]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal-buy-now]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.0d2dd661.js.map
---
+++ index.a818d6e0.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-product]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal-product]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a818d6e0.js.map
