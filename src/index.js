81dde24fef135cb7033a7ec6c049e38f1068b110
Anton Nikolaenko <anton.ck.ua@gmail.com>
2022-12-26 17:53:41 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './js/slider';
-import './js/build/snow3d.js';
+// import './js/build/snow3d.js';
 import './js/header/mobile-menu.js';
+
+import './js/modal-buy-now.js';
+import './js/modal-franchise.js';
+import './js/modal-location.js';
+import './js/modal-product.js';
+import './js/modal-readmore.js';
+
 
